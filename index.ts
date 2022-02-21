b6ee3c9c19b5dff4de71535bfba3bd8a94d09d76
ilir2523 <86475814+ilir2523@users.noreply.github.com>
2022-02-21 17:51:36 +0100

    post

--- index.ts
+++ index.ts
@@ -51,6 +51,11 @@ SELECT museums.*, json_group_array(json_object('worksId', works.id, 'workName', 
 FROM museums INNER JOIN works ON works.museumId = museums.id
 WHERE museums.id=?;`)
 
+const createMuseum = db.prepare(`INSERT INTO museums (name, city) VALUES (?,?);`)
+const createWork = db.prepare(`INSERT INTO works (name, picture, museumId) VALUES (?,?,?);`)
+
+
+
 app.get('/museums', (req, res) => {
     const museums = getAllMuseums.all()
     res.send(museums)
@@ -61,7 +66,7 @@ app.get('/museums/:id', (req, res) => {
     const museums = getMuseumById.get(id)
     if (museums) {
         res.send(museums)
-    } else res.status(404).send({error: "Museum not found."})
+    } else res.status(404).send({ error: "Museum not found." })
 })
 
 app.get('/museums-works', (req, res) => {
@@ -75,8 +80,10 @@ app.get('/museums-works', (req, res) => {
 app.get('/museums-works/:id', (req, res) => {
     const id = req.params.id
     const museums = getMuseumAndWorkJsonById.get(id)
+    if (museums.id !== null) {
         museums.works = JSON.parse(museums.works)
-    res.send(museums)
+        res.send(museums)
+    } else res.status(404).send({error: "Museum with works not found"}) 
 })
 
 app.get('/works', (req, res) => {
@@ -89,7 +96,7 @@ app.get('/works/:id', (req, res) => {
     const works = getWorkById.get(id)
     if (works) {
         res.send(works)
-    } else res.status(404).send({error: "Work not found."})
+    } else res.status(404).send({ error: "Work not found." })
 })
 
 app.get('/works-museums', (req, res) => {
@@ -100,10 +107,42 @@ app.get('/works-museums', (req, res) => {
 app.get('/works-museums/:id', (req, res) => {
     const id = req.params.id
     const museums = getWorkAndMuseumById.get(id)
-        museums.works = JSON.parse(museums.works)
+    museums.works = JSON.parse(museums.works)
     res.send(museums)
 })
 
+
+app.post('/museums', (req, res) => {
+    const { name, city } = req.body
+
+    const errors = []
+
+    if (typeof name !== 'string') errors.push(`Name missing or not a string`)
+    if (typeof city !== 'string') errors.push(`City missing or not a string`)
+
+    if (errors.length === 0) {
+        const info = createMuseum.run(name, city)
+        const newMuseum = getMuseumById.get(info.lastInsertRowid)
+        res.status(201).send(newMuseum)
+    } else res.status(400).send({ errors: errors })
+})
+
+app.post('/works', (req, res) => {
+    const { name, picture, museumId } = req.body
+
+    const errors = []
+
+    if (typeof name !== 'string') errors.push(`Name missing or not a string`)
+    if (typeof picture !== 'string') errors.push(`City missing or not a string`)
+    if (typeof museumId !== 'number') errors.push(`museumId missing or not a number`)
+
+    if (errors.length === 0) {
+        const info = createWork.run(name, picture, museumId)
+        const newWork = getWorkById.get(info.lastInsertRowid)
+        res.status(201).send(newWork)
+    } else res.status(400).send({ errors: errors })
+
+})
 
 app.listen(PORT, () => console.log(`
 Listening on: http://localhost:${PORT}
